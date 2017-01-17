d08af67900c899bb3de3bc5df1d03b719224b643
francislagueu <lagson6@yahoo.fr>
2017-01-17 14:12:58 -0800

    Project init and firebase setup and initialization

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import {AngularFireModule } from 'angularfire2';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyDGSnwVbEHLoJOqpIdnnWhejh58GPLEQwY",
+  authDomain: "angularauth-f0d4c.firebaseapp.com",
+  databaseURL: "https://angularauth-f0d4c.firebaseio.com",
+  storageBucket: "angularauth-f0d4c.appspot.com",
+  messagingSenderId: "931853890353"
+}
 
 import { AppComponent } from './app.component';
 
@@ -12,7 +21,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
